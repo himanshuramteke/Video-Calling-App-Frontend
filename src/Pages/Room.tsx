010ed5911e51fb72d9cb000ff782b17ccf3fe9f4
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-02-17 22:04:29 +0530

    Added user feed

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../Context/SocketContext";
+import UserFeedPlayer from "../Context/UserFeedPlayer";
 
 const Room: React.FC = () => {
 
     const { id } = useParams();
-    const { socket, user } = useContext(SocketContext);
-
-     const fetchParticipantList = ({roomId, participants}: {roomId: string, participants: string[]}) => {
-        console.log("Fetched room participants");
-        console.log(roomId, participants);
-     }
+    const { socket, user, stream } = useContext(SocketContext);
 
     useEffect(() => {
         // emitting this event so that either creator of room or joinee in the room 
@@ -18,13 +14,13 @@ const Room: React.FC = () => {
         if(user) {
             console.log("New user with id", user._id, "has joined the room", id);           
             socket.emit("joined-room", { roomId: id, peerId: user._id });
-            socket.on("get-users", fetchParticipantList);
         }    
     }, [id, user, socket])
 
     return (
         <div>
             room : {id}
+            <UserFeedPlayer stream={stream} />
         </div>
     )
 
