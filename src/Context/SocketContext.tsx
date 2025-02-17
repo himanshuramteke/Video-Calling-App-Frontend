d1f2b4b841f153057f30ecd96d0c044f6526cf61
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-02-17 17:11:07 +0530

    Added room creation logic

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,17 +1,31 @@
 import SocketIoClient from "socket.io-client";
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const WS_Server = "http://localhost:5500";
 
-const SocketContext = createContext<any | null>(null);
+export const SocketContext = createContext<any | null>(null);
 
-const socket = SocketIoClient(WS_Server);
+const socket = SocketIoClient(WS_Server, {
+    withCredentials: false,
+    transports: ["polling", "websocket"]
+});
 
 interface Props {
     children: React.ReactNode
 }
 
 export const SocketProvider: React.FC<Props> = ({ children }) => {
+    const navigate = useNavigate(); // will help us to programatically handle navigation 
+    useEffect(() => {
+      
+        const enterRoom = ({ roomId } : { roomId : string }) => {
+            navigate(`/room/${roomId}`);
+        }
+        //We will transfer the user to the room page when we collect an event of room-created from server   
+        socket.on("room-created", enterRoom);
+    }, []);
+
     return (
         <SocketContext.Provider value={{ socket }}>
         
